1823823d02957707f47f9bdb0aee4aec1c4dcc30
Julio Pinheiro <juliorenan@live.com>
2021-05-26 21:20:10 -0300

    fix: adicionado exemplo de EventLoop

--- stats.js
+++ stats.js
@@ -1,21 +1,26 @@
 //Common Js (require/exports)
 const os = require('os');
-const{freemem, totalmem} = os;
 
+setInterval(() => {
 
-const total = parseInt(totalmem() / 1024 / 1024);
-const mem = parseInt(freemem()/1024/1024)
-const percents = parseInt((mem/total) * 100);
+    const{freemem, totalmem} = os;
+    const total = parseInt(totalmem() / 1024 / 1024);
+    const mem = parseInt(freemem()/1024/1024)
+    const percents = parseInt((mem/total) * 100);
+    
+    // console.log( ` ${parseInt(freemem()/1024/1024)} MB`, `${parseInt(totalmem() / 1024 / 1024)} MB`);
+     const stats ={
+         free: `${mem}MB`,
+         total: `${total}MB`,
+         usage: `${percents}%`,
+     } 
+    
+    console.clear();
+    console.log('=== PC STATS ===')
+    console.table(stats);
+    
 
-// console.log( ` ${parseInt(freemem()/1024/1024)} MB`, `${parseInt(totalmem() / 1024 / 1024)} MB`);
- const stats ={
-     free: `${mem}MB`,
-     total: `${total}MB`,
-     usage: `${percents}%`,
- } 
-
-console.log('=== PC STATS ===')
-console.table(stats);
+}, 1000)
 
 
 
