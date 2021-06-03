d141a38e9f65c17928804391ed35990c9ef9dcbf
Julio Pinheiro <juliorenan@live.com>
2021-06-02 21:00:51 -0300

    modulo http

--- http/server.js
+++ http/server.js
@@ -4,19 +4,24 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer((req, res)=>{
-    const file = req.url === '/' ? 'index.html' : req.url
-  
-    // if(req.url === '/'){
-    //     fs.readFile(
-    //         path.join(__dirname, 'public', 'index.html'),
-    //         (err, content) => {
-    //             if(err){
-    //                 throw new Error;
-    //             }
-    //             res.end(content);
-    //         }
-    //     )
-    // }
+    const file = req.url === '/' ? 'index.html' : req.url;
+    const filePath = path.join(__dirname, 'public', file);
+    const extname = path.extname(filePath);
+
+    const allowedFileTypes = ['.html', '.css', '.js'];
+    const allowed = allowedFileTypes.find(item => item === extname)
+
+    if(!allowed) return;
+
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if(err){
+                throw new Error;
+            }
+            res.end(content);
+        }
+    )
 
 
     // if(req.url === '/contato'){
